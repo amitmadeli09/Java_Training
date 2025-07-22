1dc2f3226dd1d0c44227b0889a207fc8dc9e566c
Amit Madeli <amit.madeli09@gmail.com>
2025-07-22 19:23:32 +0530

    First Program

---
+++ src/Basic_Coding/firstProgram.java
@@ -0,0 +1,11 @@
+package Basic_Coding;
+
+public class firstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("My Name is Amit");
+	}
+
+}
