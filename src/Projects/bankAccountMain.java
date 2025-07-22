0fa8ce2b979bc93148a28f9890d41029bc3d40c3
Amit Madeli <amit.madeli09@gmail.com>
2025-07-22 20:56:54 +0530

    Bank Project Updated

---
+++ src/Projects/bankAccountMain.java
@@ -0,0 +1,15 @@
+package Projects;
+
+public class bankAccountMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bankAccount b = new bankAccount("Amit",1000);
+		b.deposit(500);
+		b.displayDetails();
+		b.withdraw(300);
+		b.displayDetails();
+
+	}
+
+}
