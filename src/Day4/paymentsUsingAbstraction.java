832461e433fcc9816ebccc97f64e3e0c620edb82
Amit Madeli <amit.madeli09@gmail.com>
2025-07-23 17:26:33 +0530

    Transaction Project using Interfaces and Abstraction

---
+++ src/Day4/paymentsUsingAbstraction.java
@@ -0,0 +1,46 @@
+package Day4;
+
+abstract class payments{
+	double amount;
+	
+	payments(double amount){
+		this.amount = amount;
+	}
+	abstract void makePayment();
+	
+	void showSuccessMessage() {
+		System.out.println("Rs:"+amount+" Payment Successfull");
+	}
+}
+//credit card payment 
+class creditCardPayment extends payments{
+	creditCardPayment(double amount){
+		super(amount);
+	}
+	void makePayment() {
+		System.out.println("Credit Card Payment Done Successfully");
+	}
+}
+//UPI payment
+class upiPayment extends payments{
+	upiPayment(double amount){
+		super(amount);
+	}
+	void makePayment() {
+		System.out.println("UPI Payment Done Successfully");
+	}
+}
+public class paymentsUsingAbstraction {
+	public static void main(String [] args) {
+		payments p1 = new creditCardPayment(4000);
+		p1.makePayment();
+		p1.showSuccessMessage();
+		System.out.println("Credit Card Payment Process is completed");
+		
+		payments p2 = new upiPayment(3000);
+		p2.makePayment();
+		p2.showSuccessMessage();
+		System.out.println("UPI Payment Process is completed");
+		
+	}
+}
---
+++ src/Day4/paymentsUsingInterfaces.java
@@ -0,0 +1,52 @@
+package Day4;
+
+//multiple inheritance using interfaces
+interface ecommercePayments{
+	void makePayment();
+}
+//abstract class giving the common features
+abstract class paymentSystems implements ecommercePayments{
+	double amount;
+	paymentSystems(double amount){
+		this.amount = amount;
+	}
+	void showSuccessMessage() {
+		System.out.println("Payment is Successfull "+amount);
+	}
+}
+//its using multiple inheritance from abstract class
+//for debit card
+class debitCardPayment extends paymentSystems{
+	debitCardPayment(double amount){
+		super(amount);
+	}
+	public void makePayment() {
+		System.out.println("Debit card Payment Done");
+		showSuccessMessage();
+	}
+}
+//for netbanking
+class netBankingPayment extends paymentSystems{
+	netBankingPayment(double amount){
+		super(amount);
+	}
+	public void makePayment() {
+		System.out.println("Net Banking Payment Done");
+		showSuccessMessage();
+	}
+}
+public class paymentsUsingInterfaces {
+
+	public static void main(String[] args) {
+		
+		paymentSystems p1 = new debitCardPayment(30000);
+		p1.makePayment();
+		
+		System.out.println();
+		
+		paymentSystems p2 = new netBankingPayment(55000);
+		p2.makePayment();
+
+	}
+
+}
