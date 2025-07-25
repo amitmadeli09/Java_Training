d28da68500ca5a8d4bd55e6a4642f85905090a8a
Amit Madeli <amit.madeli09@gmail.com>
2025-07-25 10:15:49 +0530

    Sorting String using Arrays class

---
+++ src/dayFive/SortNames.java
@@ -0,0 +1,17 @@
+package dayFive;
+import java.util.Arrays;
+
+public class SortNames {
+    public static void main(String[] args) {
+        String[] names = {"Subham", "Amit", "Anwesha", "Satyabrata", "Satabdi"};
+
+        // Sorting the array in ascending
+        Arrays.sort(names);
+
+        // Displaying the sorted names
+        System.out.println("Sorted Names:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
