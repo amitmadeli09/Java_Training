df8d5145e13541149292d8cc6ff8d9f282a85987
Amit Madeli <amit.madeli09@gmail.com>
2025-07-25 08:03:45 +0530

    Lambda Expression Program added successfully

---
+++ src/dayFive/LambdaExpression.java
@@ -0,0 +1,19 @@
+package dayFive;
+
+interface LambdaCalculator {
+	void operate (double a,double b);
+}
+
+public class LambdaExpression{
+	public static void main(String[] args) {
+		
+		//adding 
+		LambdaCalculator add = (double a, double b) -> System.out.println(a+b);
+		//subtracting
+		LambdaCalculator substract = (double a,double b) -> System.out.println(a-b);
+		
+		add.operate(2, 3);
+		substract.operate(4,2);
+		
+	}
+}
