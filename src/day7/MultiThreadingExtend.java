b752b89ab2f47c2285f3a2f1e672f11ec59bcf8b
Amit Madeli <amit.madeli09@gmail.com>
2025-07-28 14:50:26 +0530

    Day 7 Tasks Updated

---
+++ src/day7/MultiThreadingExtend.java
@@ -0,0 +1,48 @@
+package day7;
+
+class A extends Thread{
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Morning");
+			try {
+				Thread.sleep(10);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class B extends Thread {
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Night");
+			try {
+				Thread.sleep(10);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+				}	
+		}
+	}
+}
+
+public class MultiThreadingExtend {
+
+	public static void main(String[] args) {
+		
+		A obj1 = new A();
+		B obj2 = new B();
+		
+//		System.out.println(obj1.getPriority());
+//		obj1.setPriority(Thread.MAX_PRIORITY);
+		
+		obj1.start();
+		try {
+			Thread.sleep(5);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		obj2.start();
+
+	}
+
+}
---
+++ src/day7/MultiThreadingImplement.java
@@ -0,0 +1,48 @@
+package day7;
+
+class Multi1 implements Runnable{
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Morning");
+			try {
+				Thread.sleep(10);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class Multi2 implements Runnable {
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Night");
+			try {
+				Thread.sleep(10);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+				}	
+		}
+	}
+}
+
+public class MultiThreadingImplement {
+
+	public static void main(String[] args) {
+		
+		Runnable obj1 = new Multi1();
+		Runnable obj2 = new Multi2();
+		
+//		System.out.println(obj1.getPriority());
+//		obj1.setPriority(Thread.MAX_PRIORITY);
+		
+		Thread t1 = new Thread(obj1);
+		Thread t2 = new Thread(obj2);
+		
+		t1.start();
+		
+		t2.start();
+
+
+	}
+
+}
