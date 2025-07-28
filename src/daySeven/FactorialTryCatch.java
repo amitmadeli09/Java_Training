12fe86c873aabc743f12f32e1f873c09d3e2eed8
Amit Madeli <amit.madeli09@gmail.com>
2025-07-28 08:10:52 +0530

    Day 7 Code Updated

--- src/daySeven/FactorialTryCatch.java
+++ src/daySeven/FactorialTryCatch.java
@@ -1,16 +1,23 @@
 package daySeven;
+import java.util.Scanner;
 
 public class FactorialTryCatch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=-1;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a Number: ");
+		int num = sc.nextInt();
 		int fact=1;
 		try {
-			for(int i=1;i<=a;i++) {
-				fact=fact*i;
-			}
-			System.out.println(fact);
+			if(num < 1) {
+				throw new Exception("Input number cannot be negative: " + num);
+			}else {
+				for(int i=1 ; i<= num ; i++) {
+					fact *= i;
+					}
+				System.out.println("Result after Factorial :"+fact);
+				}
 		}
 		catch(Exception e) {
 			System.out.println("The exception is "+e);
