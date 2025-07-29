c61c6a651bf009dbe589fa00b203c8e5992da1be
Amit Madeli <amit.madeli09@gmail.com>
2025-07-29 08:17:49 +0530

    Fixing some errors

--- src/day7/FactorialTryCatch.java
+++ src/day7/FactorialTryCatch.java
@@ -4,28 +4,27 @@ import java.util.Scanner;
 public class FactorialTryCatch {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int fact = 1;
 
-        try {
+        try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter a Number: ");
             int num = sc.nextInt();
 
             if (num < 1) {
                 throw new Exception("Number can't be less than 1");
-            } else {
-                for (int i = 1; i <= num; i++) {
-                    fact *= i;
-                }
-                System.out.println("Result after Factorial: " + fact);
             }
+
+            for (int i = 1; i <= num; i++) {
+                fact *= i;
+            }
+
+            System.out.println("Result after Factorial: " + fact);
 
         } catch (Exception e) {
             System.out.println("The exception is: " + e.getMessage());
             e.printStackTrace();
-        } finally {
-            sc.close();
-            System.out.println("Scanner closed. Execution complete.");
         }
+
+        System.out.println("Execution complete.");
     }
 }
